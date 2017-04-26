23f1a129ccf97f0f85de50800ff1a75809057704
Dwayne Hoy <dwayne@roadhouse.com.au>
2017-04-26 14:56:52 +1000

    Preparation of parellel examples

---
+++ exampleRx2/src/main/java/au/com/roadhouse/rxdbflow/exampleRx2/model/ExampleDatabase.java
@@ -0,0 +1,11 @@
+package au.com.roadhouse.rxdbflow.exampleRx2.model;
+
+import com.raizlabs.android.dbflow.annotation.ConflictAction;
+import com.raizlabs.android.dbflow.annotation.Database;
+
+
+@Database(name = ExampleDatabase.NAME, version = ExampleDatabase.VERSION, insertConflict = ConflictAction.REPLACE)
+public class ExampleDatabase {
+    public static final String NAME = "example";
+    public static final int VERSION = 2;
+}
